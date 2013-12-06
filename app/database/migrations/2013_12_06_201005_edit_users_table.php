4d1c263eef8d34d88f71a8ea7b2591fc31ac20dd
Chris Violette <cvioletterun@gmail.com>
2013-12-06 15:31:25 -0500

    Users and threads migrations

---
+++ app/database/migrations/2013_12_06_201005_edit_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class EditUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function($table) {
+			$table->string('password');
+			$table->string('token')->unique();
+			$table->enum('role', array('sender', 'recipient', 'admin'))->default('admin');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function($table) {
+			$table->dropColumn(array( 'password', 'token', 'role' ));
+		});
+	}
+
+}
---
+++ app/database/migrations/2013_12_06_202029_create_threads_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThreadsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('threads', function($table) {
+			$table->increments('id');
+			$table->string('subject');
+			$table->string('token')->unique();
+			$table->integer('department_id');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('threads');
+	}
+
+}
